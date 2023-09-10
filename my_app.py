0162019968c7abe49e1cf7474d8fa828bf62f0f8
quark46 <kushovarseniy@gmail.com>
2023-09-10 19:26:57 +0700

    0.2

--- my_app.py
+++ my_app.py
@@ -1,7 +1,7 @@
-from PyQt5.QtCore import Core
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QVBoxLayout, QPushButton
 from instr import *
-app = QApplication([])
+from second_win import *
 class MainWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -11,7 +11,7 @@ class MainWidget(QWidget):
         self.show()
     def set_appear(self):
         self.setWindowTitle(txt_title)
-        self.resize(win_height, win_width)
+        self.resize(win_width, win_height)
         self.move(win_x, win_y)
     def initUI(self):
         self.hello_text = QLabel(txt_hello)
@@ -21,8 +21,12 @@ class MainWidget(QWidget):
         self.layout.addWidget(self.hello_text, alignment = Qt.AlignLeft)
         self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
         self.layout.addWidget(self.button, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout)
     def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
+        self.button.clicked.connect(self.next_click)
     def next_click(self):
         self.hide()
+        self.tw = TestWin()
+app = QApplication([])
+window1 = MainWidget()
 app.exec_()
--- second_win.py
+++ second_win.py
@@ -1,1 +1,34 @@
-class TestWin(QWid)
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QPushButton, QLineEdit
+from instr import *
+class TestWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.h_line = QHBoxLayout()
+        self.l_line = QVBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.label1 = QLabel()
+        self.label2 = QLabel()
+        self.label3 = QLabel()
+        self.label4 = QLabel()
+        self.label5 = QLabel()
+        self.label6 = QLabel()
+        self.button1 = QPushButton()
+        self.button2 = QPushButton()
+        self.button3 = QPushButton()
+        self.edit1 = QLineEdit()
+        self.edit2 = QLineEdit()
+        self.edit3 = QLineEdit()
+        self.edit4 = QLineEdit()
+        self.edit5 = QLineEdit()
+    def connects(self):
+        pass
